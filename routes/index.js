753c20b3a8a73f7dc24b643dea31b7aaec7136c1
btgolley001-ship-it <btgolley001@converse.edu>
2025-11-17 14:03:34 -0500

    Fixed "app.js" versus "index.js" GET/POST code placement. Fixed bug in teacher.pug pseudocode so it doesn't immediately crash upon trying to load the page.

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+/*router.get('/', function(req, res, next) {
   console.log('req.body (GET): '+JSON.stringify(req.body));
   res.render('index', { title: '305demo' });
+});*/
+
+router.get('/', function(req, res, next) {
+  console.log('req.query (GET): '+JSON.stringify(req.query));
+  req.body = req.query; // Now behaves very much like POST
+  res.render('index', {title: '305',
+                       formdata: req.body});
 });
 
 /* POST home page. */
-router.post('/', function(req, res, next) {
+
+/*router.post('/', function(req, res, next) {
   console.log('req.body (POST): '+JSON.stringify(req.body));
   if (req.body.HR_page) {
     console.log('HR page')
@@ -16,6 +25,20 @@ router.post('/', function(req, res, next) {
   }
   else {
     res.render('index', { title: '305demo' });
+  }
+});*/
+
+router.post('/role', function(req, res, next) {
+  console.log('req.body (POST): '+JSON.stringify(req.body));
+  
+  const { id, role } = req.body;
+
+  if (role === 'student') {
+    res.render('student', { id });
+  } else if (role === 'teacher') {
+    res.render('teacher', { id });
+  } else {
+    res.send('Invalid role');
   }
 });
 
