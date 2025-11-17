a52a90a459e605234d5dc228949e831091bbcc04
btgolley001-ship-it <btgolley001@converse.edu>
2025-11-17 14:30:11 -0500

    Formatted index's code. Now we can make selections in index, which are marshaled in renderPage(), and can then be used in the page that's rendered in its .pug file.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-
-/*router.get('/', function(req, res, next) {
-  console.log('req.body (GET): '+JSON.stringify(req.body));
-  res.render('index', { title: '305demo' });
-});*/
 
 router.get('/', function(req, res, next) {
   console.log('req.query (GET): '+JSON.stringify(req.query));
@@ -15,31 +9,115 @@ router.get('/', function(req, res, next) {
                        formdata: req.body});
 });
 
-/* POST home page. */
-
-/*router.post('/', function(req, res, next) {
-  console.log('req.body (POST): '+JSON.stringify(req.body));
-  if (req.body.HR_page) {
-    console.log('HR page')
-    res.render('HR_page', { title: 'HR page' });
-  }
-  else {
-    res.render('index', { title: '305demo' });
-  }
-});*/
 
 router.post('/role', function(req, res, next) {
   console.log('req.body (POST): '+JSON.stringify(req.body));
   
-  const { id, role } = req.body;
+  var { id, role } = req.body;
 
+  listTerms(req, res, next, id, role);
+});
+
+
+/*
+ * Unconditionally set req.app.locals.termslist to be a list of the terms 
+ * represented in the Offerings table, and call listTermCourses.
+ */
+function listTerms(req, res, next, id, role) {
+  let sql = 'SELECT distinct OffTerm, OffYear from Offering order by OffYear;'
+  req.app.locals.db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      req.app.locals.termslist = rows;
+      listTermCourses(req, res, next, id, role);
+  })
+}
+
+/*
+ * If req.body.term_year is set, set req.locals.termcourses to a list of the
+ * courses offered in that term and year.  Call listFaculty next.
+ */
+function listTermCourses(req, res, next, id, role) {
+  if (req.body.term_year) {
+    console.log(`term_year = "${req.body.term_year}"`);
+    let parts = req.body.term_year.split('_');
+    let term = parts[0];
+    let year = parts[1];
+    console.log(`term="${term}" year="${year}"`);
+    // This is all the columns in the Offering table.  The column names are
+    // specified explicitly in the SQL to control the order of the columns
+    // in the result.
+    let sql = "select OfferNo, CourseNo, OffTerm, OffYear, OffDays, OffTime, OffLocation, FacSSN from Offering where OffTerm=? and OffYear=?;";
+    req.app.locals.db.all(sql, [term, year], (err, rows) => {
+      req.app.locals.termcourses = rows;
+      console.log(`${rows.length} courses`);
+      listFaculty(req, res, next, id, role);
+    });
+  }
+  else {
+    req.app.locals.termcourses = undefined;
+    listFaculty(req, res, next, id, role);
+  }
+}
+
+/*
+ * Unconditionally set req.app.locals.facpeople to the FacSSN, FacFirstName, and
+ * FacLastName of everyone in the Faculty table.  Call inquireFaculty.
+ */
+function listFaculty(req, res, next, id, role) {  
+  let sql = 'SELECT FacSSN, FacFirstName, FacLastName from Faculty;'
+  req.app.locals.db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    req.app.locals.facpeople = rows;
+    req.app.locals.title = 
+    inquireFaculty(req, res, next, id, role);
+  })
+}
+
+/*
+ * Set req.app.locals.facDetails to all the details about a given faculty member.  Call renderPage.
+ */
+function inquireFaculty(req, res, next, id, role) {
+  if (req.body.FacSSN) {
+    sql = 'select FacSSN, FacFirstName, FacLastName, FacCity, FacState, FacZipCode, FacDept, FacRank, FacSalary, FacSupervisor, FacHireDate';
+    sql += ' from Faculty where FacSSN=?;';
+    req.app.locals.db.get(sql, [req.body.FacSSN], (err, row) => {
+      if (err) {
+        throw err;
+      }
+      req.app.locals.facDetails = row;
+      renderPage(req, res, next, id, role);
+    });
+  }
+  else {
+    req.app.locals.facDetails = undefined;
+    renderPage(req, res, next, id, role);
+  }
+}
+
+/*
+ * Marshal all the data that has been stashed in req.app.locals, and call res.render
+ * depending on what role was selected in index.
+ */
+function renderPage(req, res, next, id, role) {
   if (role === 'student') {
     res.render('student', { id });
   } else if (role === 'teacher') {
     res.render('teacher', { id });
+  } else if (role === 'registrar') {
+    res.render('registrar', { id });
   } else {
     res.send('Invalid role');
   }
-});
+  res.render('index', { title: req.app.locals.title,
+                        formdata: req.body,
+                        termslist: req.app.locals.termslist,
+                        facpeople: req.app.locals.facpeople,
+                        facdetails: req.app.locals.facDetails                        
+  });
+}
 
 module.exports = router;
